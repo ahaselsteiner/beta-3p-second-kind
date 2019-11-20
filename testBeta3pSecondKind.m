6e7474b0ad1194a5866e155fd409524c5fe39a16
Andreas <a.haselsteiner@uni-bremen.de>
2019-11-20 15:27:47 +0100

    Improve test.

--- testBeta3pSecondKind.m
+++ testBeta3pSecondKind.m
@@ -1,8 +1,7 @@
+% Test #1: Does the implementation reproduce results from the literature? 
 pdTrue = Beta3pSecondKind(0.21, 14.21, 20.34);
-
 x = [0:0.01:9];
 f = pdTrue.pdf(x);
-
 fig1 = figure('position', [100 100 450 280]);
 plot(x, f);
  message = sprintf(['In Ferreira and Soares (1999), Fig. 1b \n' ...
@@ -16,12 +15,13 @@ ylim([0 0.5]);
 box off
 
 
+% Test #2: Does the parameter estimation work correctly?
 n = 1000;
 nOfSamples = 10;
-
 alphaEstimated = nan(nOfSamples, 1);
 kEstimated = nan(nOfSamples, 1);
 nEstimated = nan(nOfSamples, 1);
+pdEstimated = Beta3pSecondKind.empty(nOfSamples, 0);
 for i = 1:nOfSamples
     sample = pdTrue.drawSample(n);
     pdEstimated(i) = Beta3pSecondKind();
@@ -38,7 +38,7 @@ plot([0.5 1.5], [0.21 0.21], '-k')
 boxplot(alphaEstimated, {'$$\hat{\alpha}$$'})
 bp = gca;
 bp.XAxis.TickLabelInterpreter = 'latex';
-text(1.15, 1, [num2str(mean(alphaEstimated), '%1.3f') '+-' ...
+text(1.15, pdTrue.Alpha, [num2str(mean(alphaEstimated), '%1.3f') '+-' ...
     num2str(std(alphaEstimated), '%1.3f')], 'fontsize', 8, ...
     'verticalalignment', 'bottom'); 
 box off
@@ -49,7 +49,7 @@ plot([0.5 1.5], [14.21 14.21], '-k')
 boxplot(kEstimated, {'$$\hat{k}$$'})
 bp = gca;
 bp.XAxis.TickLabelInterpreter = 'latex';
-text(1.15, 1, [num2str(mean(kEstimated), '%1.3f') '+-' ...
+text(1.15, pdTrue.K, [num2str(mean(kEstimated), '%1.3f') '+-' ...
     num2str(std(kEstimated), '%1.3f')], 'fontsize', 8, ...
     'verticalalignment', 'bottom');
 box off
@@ -60,8 +60,9 @@ plot([0.5 1.5], [20.34 20.34], '-k')
 boxplot(nEstimated, {'$$\hat{n}$$'})
 bp = gca;
 bp.XAxis.TickLabelInterpreter = 'latex';
-text(1.15, 2, [num2str(mean(nEstimated), '%1.3f') '+-' ...
+text(1.15, pdTrue.N, [num2str(mean(nEstimated), '%1.3f') '+-' ...
     num2str(std(nEstimated), '%1.3f')], 'fontsize', 8, ...
     'verticalalignment', 'bottom'); 
 box off
-suptitle('Parameter estimation');
+suptitle(['Parameter estimation, true parameters: ' ...
+    num2str(pdTrue.Alpha) ', ' num2str(pdTrue.K) ', ' num2str(pdTrue.N)]);
